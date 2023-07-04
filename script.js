6e4507096236da22bad8d09ffa66d22bfea163d3
Abdul Awal Anupam <78092761+abdulawalanupam@users.noreply.github.com>
2023-07-05 03:10:51 +0600

    huge changes with work

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// <!-- ========== Login button event handler ========== -->
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function () {
   // document.getElementById('login-area').style.display = 'none';
@@ -9,4 +10,40 @@ loginBtn.addEventListener("click", function () {
   ////// OR ////////
   const transactionArea = document.getElementById("transaction-area");
   transactionArea.style.display = "block";
+});
+
+// <!-- ========== Deposit button event handler ========== -->
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+  const depositAmount = document.getElementById("depositAmount").value;
+  const depositNumber = parseFloat(depositAmount);
+
+  // Error handle by me
+  if (depositAmount == "") {
+    depositNumber = 0;
+  }
+
+  /* const currentDeposit = document.getElementById("currentDeposit").innerText;
+  const currentDepositNumber = parseFloat(currentDeposit);
+  const totalDepsit = depositNumber + currentDepositNumber;
+  document.getElementById("currentDeposit").innerText = totalDepsit;
+
+  const currentBalance = document.getElementById("currentBalance").innerText;
+  const currentBalanceNumber = parseFloat(currentBalance);
+  const totalBalance = depositNumber + currentBalanceNumber;
+  document.getElementById("currentBalance").innerText = totalBalance; */
+
+  ////////// Work with single common Function //////////
+  function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const totalAmount = depositNumber + currentNumber;
+    document.getElementById(id).innerText = totalAmount;
+  }
+
+  updateSpanText("currentDeposit", depositNumber);
+  updateSpanText("currentBalance", depositNumber);
+
+  // Clear input value
+  document.getElementById("depositAmount").value = "";
 });
